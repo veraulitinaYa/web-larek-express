2944a6e1a7d02edb5f019e0526e3fad60a664933
veraulitinaYa <veraulitina@yandex.ru>
2026-04-11 17:13:53 +0300

    feat: conect to MongoDB

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,10 @@
 import express from 'express';
+import { MongoClient } from 'mongodb';
+
+const client = new MongoClient('mongodb://localhost:27017/weblarek');
+
+
+
 
 const app = express();
 const PORT = 3000;
@@ -10,3 +16,12 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Сервер работает на http://localhost:${PORT}`);
 });
+
+async function run() {
+  await client.connect();
+  const db = client.db('weblarek');
+  console.log('Подключились к серверу MongoDB');
+  const collection = db.collection("products");
+  console.log (collection);
+}
+run().catch(console.dir);
